e128b3b006ad08a9691fedf85b6789386b48c785
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-09-20 00:50:42 +0900

    impl time-trave

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -48,32 +48,40 @@ class Game extends React.Component {
     super();
     this.state = {
       history: [Array(9).fill(null)],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const squares = history[history.length - 1].slice();
     if (squares[i] || calculateWinner(squares)) return;
 
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([squares]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const squares = history[history.length - 1];
+    const squares = history[this.state.stepNumber];
     const winner = calculateWinner(squares);
 
     const moves = history.map((step, move) => {
-          console.log(move)
           const desc = move ? 'Move #' + move : 'Game start';
           return (
-            <li>
+            <li key={move}>
               <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
             </li>
           );
