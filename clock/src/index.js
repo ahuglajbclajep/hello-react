9ee0473772400bd157fd846dd3f0c6508fa82dc7
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-09-22 18:03:43 +0900

    create clock-app

---
+++ clock/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({date: new Date()});
+  }
+
+  render() {
+    return <h1>It’s {this.state.date.toLocaleTimeString()}.</h1>;
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
