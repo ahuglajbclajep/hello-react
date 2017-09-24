1fcdcaa71bcdfbd7485972d0d27abc7d9ea357fe
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-09-24 21:21:03 +0900

    impl convert func

--- thermometer/src/index.js
+++ thermometer/src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) return '';
+
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 function BoilingVerdict(props) {
   const verdict = 100 <= props.celsius ?
       'The water would boil.' : 'The water would not boil.';
