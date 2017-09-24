54933445ae6b8e49c46713eed0d7cc20705fee45
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-09-24 22:26:37 +0900

    trim logics

--- thermometer/src/index.js
+++ thermometer/src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) return '';
-
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
-}
+import {toCelsius, toFahrenheit} from './logics';
 
 function BoilingVerdict(props) {
   const verdict = 100 <= props.celsius ?
@@ -67,8 +51,8 @@ class Calculator extends React.Component {
   render() {
     const scale = this.state.scale;
     const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+    const celsius = scale === 'f' ? toCelsius(temperature) : temperature;
+    const fahrenheit = scale === 'c' ? toFahrenheit(temperature) : temperature;
 
     return (
       <div>
---
+++ thermometer/src/logics.js
@@ -0,0 +1,13 @@
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) return '';
+
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+const toCelsius = f => tryConvert(f, f => (f - 32) * 5 / 9);
+const toFahrenheit = c => tryConvert(c, c => (c * 9 / 5) + 32);
+
+export {toCelsius, toFahrenheit};
