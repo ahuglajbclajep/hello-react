3d155e1b87e7570f4ed25dead556fb91ccf0c87e
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-08-26 11:48:46 +0900

    eslint v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: { project: `${__dirname}/tsconfig.json` },
-  plugins: ["@typescript-eslint"],
   // see https://github.com/yannickcr/eslint-plugin-react/issues/1955
   settings: { react: { version: "detect" } },
+  plugins: ["@typescript-eslint", "react-hooks"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react/recommended",
     "prettier",
     "prettier/@typescript-eslint",
     "prettier/react"
   ],
   rules: {
+    // see https://reactjs.org/docs/hooks-rules
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/prefer-interface": "off", // deprecated
     "react/prop-types": "off"
   }
 };
