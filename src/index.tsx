dd3cacf17a8c049df993719078f53c64b249a900
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-05-06 15:17:19 +0900

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,21 @@
 import React, { lazy, Suspense } from "react";
 import { render } from "react-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Header } from "./components";
 import "./style.css";
 
 // see https://reactjs.org/docs/code-splitting.html#route-based-code-splitting
-const AutoHeightTextarea = lazy(() =>
-  import("./auto-height-textarea/AutoHeightTextarea")
-);
-const Counter = lazy(() => import("./counter-with-reducer/Counter"));
+const AutoHeightTextarea = lazy(() => import("./auto-height-textarea"));
+const CounterWithReducer = lazy(() => import("./counter-with-reducer"));
 
 const App: React.FC = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Header />
-      <Route path="/auto-height-textarea">
-        <AutoHeightTextarea />
-      </Route>
-      <Route path="/counter-with-reducer">
-        <Counter />
-      </Route>
+      <Switch>
+        <Route path="/auto-height-textarea" component={AutoHeightTextarea} />
+        <Route path="/counter-with-reducer" component={CounterWithReducer} />
+      </Switch>
     </Suspense>
   </Router>
 );
